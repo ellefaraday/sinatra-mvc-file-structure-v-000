01abae8aa07ebc45dad3ce298cd956df07089092
Elle Faraday <faraday.elle@gmail.com>
2018-01-21 23:59:32 +0000

    automatically backed up by learn

---
+++ app/models/dog.rb
@@ -0,0 +1,20 @@
+class Dog 
+  attr_accessor :name, :breed, :age
+
+  @@all = []
+  
+  def initialize(name, breed, age)
+    @name = name
+    @bree = breed
+    @age = age
+  end
+
+  def self.all 
+    @@all 
+  end
+
+  def save 
+    @@all << self
+  end
+end
+    
